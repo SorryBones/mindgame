33e960ec7283b91a7d500be45fc922b84300e186
Alex Bone <alexdbone99@gmail.com>
2024-06-25 07:58:49 -0400

    Created initial project.

---
+++ mindgame.py
@@ -0,0 +1,23 @@
+game_mode: int = 0
+p1Bank: int = 100
+p2Bank: int = 100
+aiBank: int = 100
+
+# Welcome message
+print("\nWelcome to Mindgame!\nSelect your mode by inputting its number.\n")
+print("1) Single Player VS AI\n2) Local 1v1\n3) Online 1v1\n")
+
+# Game mode selection
+while game_mode == 0:
+    try:
+        game_mode = int(input("> "))
+    except ValueError:
+        print("Type in the number for the game you want!")
+    if game_mode < 0 or game_mode > 3:
+        game_mode = 0
+        print("You must choose between numbers 1 - 3!")
+
+print("SELECTED MODE: " + str(game_mode))
+
+print("Thank you for selecting! Try not to lose...")
+print("~~~~~~~~~~END OF PROGRAM~~~~~~~~~~")
